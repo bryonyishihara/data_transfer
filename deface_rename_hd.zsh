4f1ef7526aedaa3012978b1717299ae05472c692
bryonyishihara <97455806+bryonyishihara@users.noreply.github.com>
2022-02-02 10:42:13 +0000

    Update and rename deface_rename.zsh to deface_rename_hd.zsh

--- deface_rename_hd.zsh
+++ deface_rename_hd.zsh
@@ -26,3 +26,16 @@ for file in *.nii.gz ; do
   else echo "done"
   fi
 done
+
+#create file with fslhd info of all nifti files
+for file in *.nii.gz ; do
+filename=$(basename ${file} .nii.gz)
+echo ${filename}
+  fslhd ${file} > ${filename}.txt
+done
+
+# combines text files and deletes original
+# && only executes second command if first is successful
+for file in *.txt ; do
+   cat ${file} >> image_hds.txt && rm ${file}
+done
