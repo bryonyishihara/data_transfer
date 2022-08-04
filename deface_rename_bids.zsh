8656b4a98f86590c86a1916c145b06b894234338
bryonyishihara <97455806+bryonyishihara@users.noreply.github.com>
2022-08-04 11:11:24 +0100

    Update deface_rename_bids.zsh

--- deface_rename_bids.zsh
+++ deface_rename_bids.zsh
@@ -1,12 +1,17 @@
 #!/bin/zsh
 
+# for DBS data with preop mri and postop CT
 #${1} = surgery date 'yyyy-mm-dd'
-# run from ses-preop directory for DBS patients with preop MRI and postop CT
+# have files sorted into ses-preop and ses-postop
+# run from DBS_dicom_nifti directory containing all sub-yyyy-mm-dd folders
 
-#defaces and renames images in bids format
+#defaces and renames images in bids format and anonymises .json files
+
+preop_path=${PWD}/sub-${1}/ses-preop
+postop_path=${PWD}/sub-${1}/ses-postop
 
 # reorient all nifti images
-for file in *.nii.gz ; do
+for file in ${preop_path}/*.nii.gz ; do
   fslreorient2std ${file}
   echo "${file} reoriented"
 done
@@ -14,75 +19,66 @@ done
 # deface reoriented T1 and FLAIR images and rename all nifti files to image type
 # moves files to anat and dwi folders
 
-mkdir anat
-mkdir dwi
+mkdir ${preop_path}/anat
+mkdir ${preop_path}/dwi
 
-for file in *.nii.gz ; do
+for file in ${preop_path}/*.nii.gz ; do
   if [[ "${file}" == *"MPRAGE"* ]] || [[ "${file}" == *"T1"* ]] ; then
-    fsl_deface ${file} sub-${1}_ses-preop_mprage.nii.gz
+    fsl_deface ${file} ${preop_path}/anat/sub-${1}_ses-preop_mprage.nii.gz
     echo "MPRAGE defaced and renamed"
-    mv sub-${1}_ses-preop_mprage.nii.gz anat
   elif [[ "${file}" == *"FLAIR"* ]] ; then
-    fsl_deface ${file} sub-${1}_ses-preop_flair.nii.gz
+    fsl_deface ${file} ${preop_path}/anat/sub-${1}_ses-preop_flair.nii.gz
     echo "FLAIR defaced and renamed"
-    mv sub-${1}_ses-preop_flair.nii.gz anat
   elif [[ "${file}" == *"T2"* ]] ; then
-    mv ${file} sub-${1}_ses-preop_t2.nii.gz
+    mv ${file} ${preop_path}/anat/sub-${1}_ses-preop_t2.nii.gz
     echo "T2 renamed"
-    mv sub-${1}_ses-preop_t2.nii.gz anat
   elif [[ "${file}" == *"FGATIR"* ]] ; then
-    mv ${file} sub-${1}_ses-preop_fgatir.nii.gz
+    mv ${file} ${preop_path}/anat/sub-${1}_ses-preop_fgatir.nii.gz
     echo "FGATIR renamed"
-    mv sub-${1}_ses-preop_fgatir.nii.gz anat
   elif [[ "${file}" == *"SWI"* ]] ; then
-    mv ${file} sub-${1}_ses-preop_swi.nii.gz
+    mv ${file} ${preop_path}/anat/sub-${1}_ses-preop_swi.nii.gz
     echo "SWI renamed"
-    mv sub-${1}_ses-preop_swi.nii.gz anat
-  else mv ${file} dwi
+  else mv ${file} ${preop_path}/dwi
   fi
 done
 
-mv *.bval dwi
-mv *.bvec dwi
+mv ${preop_path}/*.bval ${preop_path}/dwi
+mv ${preop_path}/*.bvec ${preop_path}/dwi
 
 # anonymises and renames .json files
 
-for file in *.json ; do
+for file in ${preop_path}/*.json ; do
   if [[ "${file}" == *"MPRAGE"* ]] || [[ "${file}" == *"T1"* ]] ; then
-    jq "del(.PatientName, .PatientID, .AccessionNumber, .PatientBirthDate, .PatientSex, .PatientWeight)" ${file} > sub-${1}_ses-preop_mprage.json && rm ${file}
+    jq "del(.PatientName, .PatientID, .AccessionNumber, .PatientBirthDate, .PatientSex, .PatientWeight)" ${file} > ${preop_path}/anat/sub-${1}_ses-preop_mprage.json && rm ${file}
     echo "${file} anonymised"
-    mv sub-${1}_ses-preop_mprage.json anat
   elif [[ "${file}" == *"FLAIR"* ]] ; then
-    jq "del(.PatientName, .PatientID, .AccessionNumber, .PatientBirthDate, .PatientSex, .PatientWeight)" ${file} > sub-${1}_ses-preop_flair.json && rm ${file}
+    jq "del(.PatientName, .PatientID, .AccessionNumber, .PatientBirthDate, .PatientSex, .PatientWeight)" ${file} > ${preop_path}/anat/sub-${1}_ses-preop_flair.json && rm ${file}
     echo "${file} anonymised"
-    mv sub-${1}_ses-preop_flair.json anat
   elif [[ "${file}" == *"T2"* ]] ; then
-    jq "del(.PatientName, .PatientID, .AccessionNumber, .PatientBirthDate, .PatientSex, .PatientWeight)" ${file} > sub-${1}_ses-preop_t2.json && rm ${file}
+    jq "del(.PatientName, .PatientID, .AccessionNumber, .PatientBirthDate, .PatientSex, .PatientWeight)" ${file} > ${preop_path}/anat/sub-${1}_ses-preop_t2.json && rm ${file}
     echo "${file} anonymised"
-    mv sub-${1}_ses-preop_t2.json anat
   elif [[ "${file}" == *"FGATIR"* ]] ; then
-    jq "del(.PatientName, .PatientID, .AccessionNumber, .PatientBirthDate, .PatientSex, .PatientWeight)" ${file} > sub-${1}_ses-preop_fgatir.json && rm ${file}
+    jq "del(.PatientName, .PatientID, .AccessionNumber, .PatientBirthDate, .PatientSex, .PatientWeight)" ${file} > ${preop_path}/anat/sub-${1}_ses-preop_fgatir.json && rm ${file}
     echo "${file} anonymised"
-    mv sub-${1}_ses-preop_fgatir.json anat
   elif [[ "${file}" == *"SWI"* ]] ; then
-    jq "del(.PatientName, .PatientID, .AccessionNumber, .PatientBirthDate, .PatientSex, .PatientWeight)" ${file} > sub-${1}_ses-preop_swi.json && rm ${file}
+    jq "del(.PatientName, .PatientID, .AccessionNumber, .PatientBirthDate, .PatientSex, .PatientWeight)" ${file} > ${preop_path}/anat/sub-${1}_ses-preop_swi.json && rm ${file}
     echo "${file} anonymised"
-    mv sub-${1}_ses-preop_swi.json anat
-  else mv ${file} dwi
+  else mv ${file} ${preop_path}/dwi
   fi
 done
 
-for file in $PWD/dwi/*.json ; do
-    jq "del(.PatientName, .PatientID, .AccessionNumber, .PatientBirthDate, .PatientSex, .PatientWeight)" ${file} > $PWD/dwi/"${file/PAT01/}" && rm ${file}
+for file in ${preop_path}/dwi/*.json ; do
+    jq "del(.PatientName, .PatientID, .AccessionNumber, .PatientBirthDate, .PatientSex, .PatientWeight)" ${file} > ${preop_path}/dwi/"${file/PAT01_/}" && rm ${file}
     echo "${file} anonymised"
 done
 
-for file in $PWD/../ses-postop/*.json ; do
-  jq "del(.PatientName, .PatientID, .AccessionNumber, .PatientBirthDate, .PatientSex, .PatientWeight)" ${file} > $PWD/../ses-postop/sub-${1}_ses-postop_CT.json && rm ${file}
+for file in ${postop_path}/*.json ; do
+  jq "del(.PatientName, .PatientID, .AccessionNumber, .PatientBirthDate, .PatientSex, .PatientWeight)" ${file} > ${postop_path}/sub-${1}_ses-postop_CT.json && rm ${file}
   echo "${file} anonymised"
 done
 
-for file in $PWD/../ses-postop/*.nii.gz ; do
-    mv ${file} $PWD/../ses-postop/sub-${1}_ses-postop_CT.nii.gz
+for file in ${postop_path}/*.nii.gz ; do
+    mv ${file} ${postop_path}/sub-${1}_ses-postop_CT.nii.gz
     echo "${file} renamed"
 done
+
