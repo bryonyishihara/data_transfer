2b393d7555d87f11763c1a56784aeac1f4bad26d
bryonyishihara <97455806+bryonyishihara@users.noreply.github.com>
2022-08-04 11:12:42 +0100

    Add files via upload

---
+++ preprocessing.zsh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+# calls deface_rename_bids.zsh and runs on all patients in DBS_dicom_nifti directory
+# patient folders need to be sorted into ses-preop and ses-postop
+# input all of the surgery dates in the directory yyyy-mm-dd
+
+for var in "$@" ; do
+    echo "${var}"
+    /Users/bryony/Documents/GitHub/Data_transfer/data_transfer/deface_rename_bids.zsh ${var}
+    echo "${var} preprocessing done"
+done
