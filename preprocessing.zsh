e26087c5dafe8b83a62bf9186d306c175f1db16c
bryonyishihara <97455806+bryonyishihara@users.noreply.github.com>
2023-01-25 14:43:25 +0000

    Update preprocessing.zsh

--- preprocessing.zsh
+++ preprocessing.zsh
@@ -2,7 +2,7 @@
 
 # calls deface_rename_bids.zsh and runs on all patients in DBS_dicom_nifti directory
 # patient folders need to be sorted into ses-preop and ses-postop
-# input all of the surgery dates in the directory yyyy-mm-dd
+# input all subject IDs
 
 for var in "$@" ; do
     echo "${var}"
